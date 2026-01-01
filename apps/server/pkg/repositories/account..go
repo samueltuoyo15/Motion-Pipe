ea8d669dc20dee005aa0429c9f7eddb25cbca00c
samueltuoyo15 <samueltuoyo9082@gmail.com>
2026-01-01 13:01:48 +0100

    feat(auth): add account registration and lookup

--- apps/server/pkg/repositories/account..go
+++ apps/server/pkg/repositories/account..go
@@ -1,6 +1,10 @@
 package repositories
 
 import (
+	"context"
+	"errors"
+	"motion-pipe/pkg/entities"
+
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -10,4 +14,54 @@ type AccountRepository struct {
 
 func NewAccountRepository(db *pgxpool.Pool) *AccountRepository {
 	return &AccountRepository{DB: db}
+}
+
+func (accountRepo *AccountRepository) Register(ctx context.Context, account *entities.Account) error {
+	query := `
+	INSERT INTO accounts (name, email, provider, provider_id, created_at, updated_at
+	VALUES ($1, $2, $3, $4, now(), now())
+	RETURNING id, created_at, updated_at
+	`
+
+	err := accountRepo.DB.QueryRow(ctx, query, account.Name, account.Email, account.Provider, account.ProviderID).Scan(&account.ID, &account.CreatedAt, &account.UpdatedAt)
+
+	return err
+}
+
+func (accountRepo *AccountRepository) GetAccountByProviderID(ctx context.Context, provider, providerID string) (*entities.Account, error) {
+	query := `
+		SELECT id, name, email, provider, provider_id, created_at, updated_at
+		FROM accounts
+		WHERE provider=$1 AND provider_id=$2
+	`
+	account := &entities.Account{}
+	err := accountRepo.DB.QueryRow(ctx, query, provider, providerID).Scan(
+		&account.ID,
+		&account.Name,
+		&account.Email,
+		&account.Provider,
+		&account.ProviderID,
+		&account.CreatedAt,
+		&account.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return account, nil
+}
+
+func (accountRepo *AccountRepository) FindOrCreate(ctx context.Context, account *entities.Account) (*entities.Account, error) {
+	existing, err := accountRepo.GetAccountByProviderID(ctx, account.Provider, account.ProviderID)
+	if err == nil {
+		return existing, nil
+	}
+
+	if errors.Is(err, pgxpool.ErrNoRows) {
+		if err := accountRepo.Register(ctx, account); err != nil {
+			return nil, err
+		}
+		return account, nil
+	}
+
+	return nil, err
 }
